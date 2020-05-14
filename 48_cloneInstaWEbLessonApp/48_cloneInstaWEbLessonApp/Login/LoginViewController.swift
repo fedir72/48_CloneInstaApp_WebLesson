4795323fc221167ee645be5c4d4070bb5bf17692
fedir72 <fedir72@ua.fm>
2020-05-14 14:26:17 +0300

    empty project and settings for scenedelegate

---
+++ 48_cloneInstaWEbLessonApp/48_cloneInstaWEbLessonApp/Login/LoginViewController.swift
@@ -0,0 +1,22 @@
+//
+//  LoginViewController.swift
+//  48_cloneInstaWEbLessonApp
+//
+//  Created by fedir on 14.05.2020.
+//  Copyright © 2020 fedir. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)
+    }
+    
+
+  
+
+}
